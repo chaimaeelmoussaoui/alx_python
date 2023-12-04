3e717881ba4fde4dcfeaee5146a11174fa2c2813
Chaimae El Moussaoui <elmoussaouichaimae1997@gmail.com>
2023-12-04 15:02:19 +0000

    0

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,13 +1,1 @@
-def print_line(char, length):
-    return char * length
-
-def main():
-    length = 40
-    char = '*'
-
-    print(print_line(char, length))
-    print("Programming is like building a multilingual puzzle")
-    print(print_line(char, length))
-
-if __name__ == "__main__":
-    main()
+print("\"Programming is like building a multilingual puzzle")
