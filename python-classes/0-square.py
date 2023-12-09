d39d1a2ee38e236ebead94c7218c1c65172ce981
Chaimae El Moussaoui <elmoussaouichaimae1997@gmail.com>
2023-12-09 21:59:11 +0000

    00

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,11 @@
 class Square:
     def __init__(self, size):
         self.__size = size
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, size):
+        if size < 0:
+            raise ValueError("Size must be non-negative")
+        self.__size = size
